4570e8d4c541e45f2090b8b37a8c9c22509d3ed8
emmanuel <ejariza@gmail.com>
2020-01-11 21:05:36 -0500

    Updates

---
+++ precache-manifest.354193e4cc4064e8f1fbdaf9b2b0601a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e7366261fb9274c23c52266f47b41f4",
+    "url": "/drumixfy/index.html"
+  },
+  {
+    "revision": "016ff225f4890cc539dc",
+    "url": "/drumixfy/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "cb53587f4c9a5ab98c27",
+    "url": "/drumixfy/static/css/main.7b802e29.chunk.css"
+  },
+  {
+    "revision": "016ff225f4890cc539dc",
+    "url": "/drumixfy/static/js/2.6fe6f7b9.chunk.js"
+  },
+  {
+    "revision": "6927c6c2aa28378d514655499e49ee93",
+    "url": "/drumixfy/static/js/2.6fe6f7b9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cb53587f4c9a5ab98c27",
+    "url": "/drumixfy/static/js/main.01638526.chunk.js"
+  },
+  {
+    "revision": "82d4b9db48d31f2a521df520714fec97",
+    "url": "/drumixfy/static/js/main.01638526.chunk.js.LICENSE"
+  },
+  {
+    "revision": "52ac0a24df0ccce26fe6",
+    "url": "/drumixfy/static/js/runtime-main.5480629c.js"
+  }
+]);
